2b831cb9f3545d4f3b69a61de7f47a047c900cbb
Artem Moshnin <tap.kap.tap@gmail.com>
2020-11-27 20:30:08 +0100

    S

--- RJS-QuickEat/src/index.tsx
+++ RJS-QuickEat/src/index.tsx
@@ -1,11 +1,41 @@
+// PLUGINS IMPORTS //
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./Content/App"
 import serviceworker from "./serviceworker"
+
+// FIREBASE SETTINGS //
+import firebase from "firebase/app"
+import { createFirestoreInstance } from "redux-firestore"
+import { ReactReduxFirebaseProvider } from "react-redux-firebase"
+
+// REDUX SETTINGS //
+import ReduxStore from "Redux/ReduxStore"
+import { Provider } from "react-redux"
+
+// COMPONENTS IMPORTS //
+import App from "./Content/App"
+
+/////////////////////////////////////////////////////////////////////////////
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true,
+}
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: ReduxStore.dispatch,
+  createFirestoreInstance,
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ReduxStore}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 )
